5e5173e581f957f047c9d8b4f65a486245e3a2fe
duyuqian <15738381799@163.com>
2020-02-29 12:19:24 +0800

    [duyuqian] feat: create the Game class with chances field and begin method to control the guess number game

---
+++ src/main/java/com/thoughtworks/game/Game.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.game;
+
+import java.util.Scanner;
+
+public class Game {
+    int chances;
+
+    public Game() {
+    }
+
+    public Game(int chances) {
+        this.chances = chances;
+    }
+
+    public void begin(GameAnswer gameAnswer) {
+        String res = "";
+        for (int i = 0; i < this.chances; i++) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (gameAnswer.compareWithGameAnswer(input).length() == 0) {
+                System.out.println("Congratulations, you win!");
+                break;
+            }
+            if (gameAnswer.compareWithGameAnswer(input).length() != 4) {
+                i -= 1;
+            }
+            res += input + "\t" + gameAnswer.compareWithGameAnswer(input) + "\n";
+            System.out.print(res);
+            if (i == this.chances - 1) {
+                System.out.println("Unfortunately, you have no chance, the answer is " + gameAnswer + "!");
+            }
+        }
+    }
+}
